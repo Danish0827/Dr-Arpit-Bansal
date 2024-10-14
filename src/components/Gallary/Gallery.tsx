07487df421781eba33d27ebfd81faba3375090ea
Danish <s.danish0827@gmail.com>
2024-10-14 17:43:13 +0530

    all changes done with backendfkjk

--- src/components/Gallary/Gallery.tsx
+++ src/components/Gallary/Gallery.tsx
@@ -78,7 +78,13 @@ const Gallery = () => {
         <div className=" grid-cols-1 gap-5 flex justify-center lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
           {videoData?.map((item: any) => (
             <div className="space-y-2 lg:w-1/4 md:w-1/3 w-1/2" key={item.id}>
-              <div className="px-0 border-2 border-solid border-[#232c77] mb-5 rounded-[10px]">
+              <div
+                style={{
+                  boxShadow:
+                    "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
+                }}
+                className="px-0 border-2 border-solid border-[#fff] mb-5 rounded-[10px]"
+              >
                 <div className="relative">
                   <img
                     className="m-auto rounded-lg"
@@ -87,7 +93,8 @@ const Gallery = () => {
                     src={item.img.src}
                     alt=""
                     style={{
-                      boxShadow: 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px'
+                      boxShadow:
+                        "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
                     }}
                   />
                   <h4 className="text-white text-xl font-semibold text-center py-2 absolute top-0 w-full h-full flex items-center justify-center rounded-lg bg-[#00000066]">
@@ -134,9 +141,6 @@ const Gallery = () => {
               {" "}
               <Image.PreviewGroup>
                 <Image
-                style={{
-                  boxShadow: 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px'
-                }}
                   src={item.image.src} // Access the src from the image object
                   alt={item.heading}
                   className="rounded-lg"
